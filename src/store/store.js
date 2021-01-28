6175a031b5147487f6e2f78fced351593fe0f9d5
rizqitsani <rizqitsani@gmail.com>
2021-01-28 14:12:57 +0700

    [Back][]: fix redux-firestore config

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,25 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+import firebase from '../config/firebase';
 import { getFirebase } from 'react-redux-firebase';
-import { getFireStore } from 'redux-firestore';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers';
 
-const middlewares = [thunk.withExtraArgument({ getFirebase, getFireStore })];
+const middlewares = [thunk.withExtraArgument({ getFirebase, getFirestore })];
 const middlewareEnhancer = applyMiddleware(...middlewares);
 
-const enhancers = [middlewareEnhancer];
+const enhancers = [middlewareEnhancer, reduxFirestore(firebase)];
 const composedEnhancers = composeWithDevTools(...enhancers);
 
 const store = createStore(rootReducer, composedEnhancers);
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(...middleware)),
+// );
 
 export default store;
