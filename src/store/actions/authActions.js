8336343a53cae4df11c74a34f50c8bf7fa0303c5
rizqitsani <rizqitsani@gmail.com>
2021-01-29 17:20:58 +0700

    [Back][]: change authActions to async await

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,6 +1,7 @@
 import {
   SIGN_IN_FAIL,
   SIGN_IN_SUCCESS,
+  SIGN_OUT_FAIL,
   SIGN_OUT_SUCCESS,
   SIGN_UP_FAIL,
   SIGN_UP_SUCCESS,
@@ -8,105 +9,100 @@ import {
   UPDATE_PROFILE_SUCCESS,
 } from '../actions/types';
 
-export const signIn = (credentials) => (
+export const signIn = (credentials) => async (
   dispatch,
   getState,
   { getFirebase },
 ) => {
-  const firebase = getFirebase();
+  try {
+    const firebase = getFirebase();
 
-  const { email, password } = credentials;
+    const { email, password } = credentials;
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(() => {
-      dispatch({ type: SIGN_IN_SUCCESS });
-    })
-    .catch((error) => {
-      dispatch({ type: SIGN_IN_FAIL, payload: error });
-    });
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+
+    dispatch({ type: SIGN_IN_SUCCESS });
+  } catch (error) {
+    dispatch({ type: SIGN_IN_FAIL, payload: error });
+  }
 };
 
-export const signUp = (newUserData) => (
+export const signUp = (newUserData) => async (
   dispatch,
   getState,
   { getFirebase, getFirestore },
 ) => {
-  const firebase = getFirebase();
-  const firestore = getFirestore();
+  try {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
 
-  const { name, email, password } = newUserData;
+    const { name, email, password } = newUserData;
 
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((response) => {
-      return firestore.collection('users').doc(response.user.uid).set({
-        name,
-      });
-    })
-    .then(() => {
-      dispatch({ type: SIGN_UP_SUCCESS });
-    })
-    .catch((error) => {
-      dispatch({ type: SIGN_UP_FAIL, payload: error });
+    const response = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+
+    await firestore.collection('users').doc(response.user.uid).set({
+      name,
     });
+
+    dispatch({ type: SIGN_UP_SUCCESS });
+  } catch (error) {
+    dispatch({ type: SIGN_UP_FAIL, payload: error });
+  }
 };
 
-export const signOut = () => (dispatch, getState, { getFirebase }) => {
-  const firebase = getFirebase();
+export const signOut = () => async (dispatch, getState, { getFirebase }) => {
+  try {
+    const firebase = getFirebase();
 
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
-      dispatch({ type: SIGN_OUT_SUCCESS });
-    })
-    .catch((error) => {
-      // An error happened.
-    });
+    await firebase.auth().signOut();
+
+    dispatch({ type: SIGN_OUT_SUCCESS });
+  } catch (error) {
+    dispatch({ type: SIGN_OUT_FAIL, payload: error });
+  }
 };
 
-export const updateProfile = (updatedUserData) => (
+export const updateProfile = (updatedUserData) => async (
   dispatch,
   getState,
   { getFirebase, getFirestore },
 ) => {
-  const firebase = getFirebase();
-  const firestore = getFirestore();
+  try {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
 
-  const { name: currentUserName } = getState().firebase.profile;
-  const {
-    uid: currentUserID,
-    email: currentUserEmail,
-  } = getState().firebase.auth;
+    const { name: currentUserName } = getState().firebase.profile;
+    const {
+      uid: currentUserID,
+      email: currentUserEmail,
+    } = getState().firebase.auth;
 
-  const { name, email, newPassword } = updatedUserData;
+    const { name, email, newPassword } = updatedUserData;
 
-  const promises = [];
+    const promises = [];
 
-  if (name !== currentUserName) {
-    promises.push(
-      firestore.collection('users').doc(currentUserID).set({
-        name,
-      }),
-    );
+    if (name !== currentUserName) {
+      promises.push(
+        firestore.collection('users').doc(currentUserID).set({
+          name,
+        }),
+      );
+    }
+
+    if (email !== currentUserEmail) {
+      promises.push(firebase.auth().currentUser.updateEmail(email));
+    }
+
+    if (newPassword.length) {
+      promises.push(firebase.auth().currentUser.updatePassword(newPassword));
+    }
+
+    await Promise.all(promises);
+
+    dispatch({ type: UPDATE_PROFILE_SUCCESS });
+  } catch (error) {
+    dispatch({ type: UPDATE_PROFILE_FAIL, payload: error });
   }
-
-  if (email !== currentUserEmail) {
-    promises.push(firebase.auth().currentUser.updateEmail(email));
-  }
-
-  if (newPassword.length) {
-    promises.push(firebase.auth().currentUser.updatePassword(newPassword));
-  }
-
-  Promise.all(promises)
-    .then(() => {
-      dispatch({ type: UPDATE_PROFILE_SUCCESS });
-    })
-    .catch((error) => {
-      dispatch({ type: UPDATE_PROFILE_FAIL, payload: error });
-    });
 };
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,7 @@
 import {
   SIGN_IN_FAIL,
   SIGN_IN_SUCCESS,
+  SIGN_OUT_FAIL,
   SIGN_OUT_SUCCESS,
   SIGN_UP_FAIL,
   SIGN_UP_SUCCESS,
@@ -17,12 +18,13 @@ const authReducer = (state = initialState, action) => {
 
   switch (type) {
     case SIGN_IN_FAIL:
+    case SIGN_OUT_FAIL:
     case SIGN_UP_FAIL:
     case UPDATE_PROFILE_FAIL:
       return { ...state, error: payload?.message };
     case SIGN_IN_SUCCESS:
-    case SIGN_UP_SUCCESS:
     case SIGN_OUT_SUCCESS:
+    case SIGN_UP_SUCCESS:
     case UPDATE_PROFILE_SUCCESS:
       return { ...state, error: null };
     default:
