bdb1efb844801daece36c7164dd6daf3f2c4c900
rizqitsani <rizqitsani@gmail.com>
2021-01-31 10:58:14 +0700

    [Front][-]: remove all log

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -2,9 +2,6 @@ export const register = () => {
   const swURL = `${process.env.PUBLIC_URL}/sw.js`;
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register(swURL)
-      .then(() => console.log('service worker registered'))
-      .catch(() => console.log('service worker not registered'));
+    navigator.serviceWorker.register(swURL);
   }
 };
